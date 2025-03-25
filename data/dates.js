23646651937a9df85613d1d3520e2c2da919f80c
esaytar <usovaalena01@gmail.com>
2025-03-25 12:00:15 +0400

    optimized dates

--- data/dates.js
+++ data/dates.js
@@ -17,10 +17,10 @@ export const dates = [
     { reason: 'Рыбий день', date: "2024-08-29 00:00:00" },
     { reason: 'Годовщина', date: "2024-09-06 00:00:00" }, 
     { reason: '23 февраля', date: "2025-02-23 00:00:00" },*/
-    // { reason: 'Ближайший звонок', date: nearestCall },
-    { reason: 'Повестка', date: "2024-07-02 08:00:00" },
+    // { reason: 'Ближайший звонок', date: nearestCall },   
+    // { reason: 'Повестка', date: "2024-07-02 08:00:00" },
+    // { reason: 'Присяга', date: "2024-08-02 09:00:00" },
     { reason: 'Призыв', date: CALL_DATE },
-    { reason: 'Присяга', date: "2024-08-02 09:00:00" },
     { reason: '300 дней до дембеля', date: getDaysTil(300) },
     { reason: 'Четверть службы', date: getQuarterOfTheYear() },
     { reason: '100 дней после призыва', date: getDaysTil(265) },
@@ -32,14 +32,14 @@ export const dates = [
     { reason: 'Четверть до дембеля', date: getLastQuarter() },
     { reason: '300 дней после призыва', date: getDaysTil(65) },
     { reason: 'Дембель', date: DEM_DATE },
-    (monthsCount !== 12 && monthsCount !== 0 && new Date() <= new Date(DEM_DATE)) 
+    (nextMonthDate.toLocaleString() !== new Date(DEM_DATE).toLocaleDateString()) 
         && { reason: `${monthsCount} месяцев службы`, date: nextMonthDate }
 ]
 
 export function printMonths() {
     const start = new Date(CALL_DATE)
     const end = new Date(DEM_DATE)
-    let point = new Date(start)
+    let point = new Date(start) 
 
     const options = { year: 'numeric', month: 'long', day: 'numeric'}
 
@@ -94,27 +94,17 @@ function getNextSunday() {
 
 function getNextMonth(data) {
     const today = new Date()
-    const nextMonthDate = new Date()
-    let monthsCount = 0
+    const nextMonthDate = new Date(today)
+    const stringData = new Date(data)
 
-    function findMonthsCount(step, sign) {
-        switch (sign) {
-            case '-':
-                return Math.abs(new Date().getMonth() - new Date(data).getMonth()) + step 
-            case '+':
-                return Math.abs(new Date().getMonth() + new Date(data).getMonth()) + step
-            default:
-                return
-        }
-    }
+    let monthsCount = 12 - ((stringData.getFullYear() - today.getFullYear()) * 12 + (stringData.getMonth() - today.getMonth()))
+    nextMonthDate.setMonth(today.getMonth() + 1)
+    nextMonthDate.setDate(stringData.getDate())
 
-    if (today.getDate() < new Date(DEM_DATE).getDate()) {        
-        nextMonthDate.setMonth(today.getMonth(), new Date(DEM_DATE).getDate())
-        monthsCount = new Date(data).getFullYear() !== new Date().getFullYear() ? findMonthsCount(0, '-') : findMonthsCount(0, '+')
-    } else {
-        nextMonthDate.setMonth(today.getMonth() + 1, new Date(DEM_DATE).getDate())
-        monthsCount = new Date(data).getFullYear() !== new Date().getFullYear() ? findMonthsCount(1, '-') : findMonthsCount(1, '+')
-    }
+    if (stringData.getDate() > today.getDate()) {
+        nextMonthDate.setMonth(today.getMonth())
+    } else monthsCount++
+
     nextMonthDate.setHours(0, 0, 0, 0)
 
     return [nextMonthDate, monthsCount]
